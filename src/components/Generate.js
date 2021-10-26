7b8c59461eabd9e196c5c4ed512a051547e19250
ErikHartman <erik.hartman@hotmail.com>
2021-10-26 21:42:54 +0200

    update

--- src/components/Generate.js
+++ src/components/Generate.js
@@ -1,19 +1,19 @@
-import React from "react";
 import "./components.css";
 
-import { AwesomeButtonProgress } from "react-awesome-button";
-import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
+import { AwesomeButton } from "react-awesome-button";
 
 function Generate() {
   return (
-    <AwesomeButtonProgress
-      cssModule={AwesomeButtonStyles}
-      type="secondary"
-      size="large"
-      action={(element, next) => console.log(next)}
-    >
-      Generate
-    </AwesomeButtonProgress>
+    <div>
+      <AwesomeButton
+        className="generate-button"
+        onPress={() => {
+          console.log("hello");
+        }}
+      >
+        Generate
+      </AwesomeButton>
+    </div>
   );
 }
 
