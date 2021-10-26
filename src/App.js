bd29c5f5f81271895a64f2ebbed91178be29220d
ErikHartman <erik.hartman@hotmail.com>
2021-10-26 08:40:14 +0200

    circular-slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CircularSlider from "@fseehawer/react-circular-slider";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Waddup biiitch
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hur taggad?</p>
+        <CircularSlider
+          label="tagg-skala"
+          labelColor="#005a58"
+          knobColor="#005a58"
+          progressColorFrom="#00bfbd"
+          progressColorTo="#009c9a"
+          progressSize={24}
+          trackColor="#eeeeee"
+          trackSize={24}
+          data={[...Array(100).keys()]} //...
+          dataIndex={10}
+          onChange={(value) => {
+            console.log(value);
+          }}
+        />
       </header>
     </div>
   );
